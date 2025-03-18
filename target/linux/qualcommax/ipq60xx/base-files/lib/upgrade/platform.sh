83f6ecae06fbe6ad841e4c1e128c2752c78eb57a
277598414 <277598414@qq.com>
2025-03-19 07:56:25 +0800

    Add files via upload
    Signed-off-by: 277598414 <277598414@qq.com>

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -37,6 +37,11 @@ platform_do_upgrade() {
 		fw_setenv bootcount 0
 		nand_do_upgrade "$1"
 		;;
+		jdc,ax1800-pro)
+		kernelname="0:HLOS"
+		rootfsname="rootfs"
+		mmc_do_upgrade "$1"
+		;;
 	netgear,wax214)
 		nand_do_upgrade "$1"
 		;;
